38c1e7e8231814cbc934530cc4883bde680ae05a
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2018-02-17 19:35:49 +0200

    lesson 3

---
+++ src/com/javaguru/lesson3/DoWhileExample.java
@@ -0,0 +1,12 @@
+package com.javaguru.lesson3;
+
+public class DoWhileExample {
+
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 101);
+    }
+}
