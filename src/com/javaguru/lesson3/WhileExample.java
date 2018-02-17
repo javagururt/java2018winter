d015eeb1ac60a2773b104f556898a0065c304c8f
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2018-02-17 19:33:02 +0200

    lesson 3

---
+++ src/com/javaguru/lesson3/WhileExample.java
@@ -0,0 +1,12 @@
+package com.javaguru.lesson3;
+
+public class WhileExample {
+
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 101) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
