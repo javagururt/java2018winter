99ab63a9c9f655e48c567a5ea81ba9cbf12009d0
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2018-02-17 19:30:03 +0200

    lesson 3

---
+++ src/com/javaguru/lesson3/ExampleFor.java
@@ -0,0 +1,10 @@
+package com.javaguru.lesson3;
+
+public class ExampleFor {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 101; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
