c1c6895ba3e6e20cc5d26a975e3a9d07312d7a25
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2018-02-17 19:27:53 +0200

    lesson 3

---
+++ src/com/javaguru/lesson3/Increment.java
@@ -0,0 +1,12 @@
+package com.javaguru.lesson3;
+
+public class Increment {
+
+    public static void main(String[] args) {
+        int counter = 0;
+        counter++;
+        counter++;
+        System.out.println(counter++);
+        System.out.println(++counter);
+    }
+}
