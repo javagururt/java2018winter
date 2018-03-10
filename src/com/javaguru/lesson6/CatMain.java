a5c9d4f37d2a5db038f7eacad3c6aaa255c03d12
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2018-03-10 19:32:53 +0200

    Cats

---
+++ src/com/javaguru/lesson6/CatMain.java
@@ -0,0 +1,18 @@
+package com.javaguru.lesson6;
+
+class CatMain {
+
+    public static void main(String[] args) {
+        Cat cat = new Cat("Black");
+        System.out.println(cat);
+        changeColor(cat);
+        System.out.println(cat);
+    }
+
+    public static void changeColor(Cat cat) {
+//        cat.setColor("Red");
+        Cat secondCat = cat;
+        secondCat.setColor("Red");
+
+    }
+}
