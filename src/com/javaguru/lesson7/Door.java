cff12b25b579e0a7dad21cd30efdbd2724eeba1c
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2018-03-17 19:45:43 +0200

    Equals and Hashcode

---
+++ src/com/javaguru/lesson7/Door.java
@@ -0,0 +1,61 @@
+package com.javaguru.lesson7;
+
+import java.util.Objects;
+
+class Door {
+
+    private String color;
+    private String material;
+    private String size;
+    private String manufacturer;
+
+
+    Door(String color, String material, String size, String manufacturer) {
+        this.color = color;
+        this.material = material;
+        this.size = size;
+        this.manufacturer = manufacturer;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    @Override
+    public String toString() {
+        return "Door{" +
+                "color='" + color + '\'' +
+                ", material='" + material + '\'' +
+                ", size='" + size + '\'' +
+                ", manufacturer='" + manufacturer + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Door door = (Door) o;
+        return Objects.equals(color, door.color) &&
+                Objects.equals(material, door.material) &&
+                Objects.equals(size, door.size) &&
+                Objects.equals(manufacturer, door.manufacturer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(color, material, size, manufacturer);
+    }
+}
---
+++ src/com/javaguru/lesson7/DoorMain.java
@@ -0,0 +1,23 @@
+package com.javaguru.lesson7;
+
+class DoorMain {
+
+    public static void main(String[] args) {
+        Door firstDoor = new Door("Black",
+                "Carbon",
+                "50x50",
+                "Philips");
+
+        Door secondDoor = new Door("Black",
+                "Carbon",
+                "50x50",
+                "Philips");
+
+        if (firstDoor.equals(secondDoor)) {
+            System.out.println("Doors are equals");
+        } else {
+            System.out.println("Doors are different");
+        }
+
+    }
+}
